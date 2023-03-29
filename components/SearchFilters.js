617f01ebbc73e31a39584fac5581b3ceee441430
kaiesmahmud <kaiesmahmud@gmail.com>
2023-03-29 15:31:24 +0600

    filterData file added from jsMartery

--- components/SearchFilters.js
+++ components/SearchFilters.js
@@ -1,20 +1,24 @@
 'use client'
 import React, { useEffect, useState } from 'react'
+
 //chakra ui
 import { Box, Flex, Text, Icon, Select, Spinner } from '@chakra-ui/react'
+
 //Next element
 import Image from 'next/image'
 import { useRouter } from 'next/router'
+
 //react-icons
 import {MdCancel} from 'react-icons/md'
 
 
 const SearchFilters = () => {
     const rounter = useRouter();
+    const [filters, setFilters] = useState({})
   return (
-    <div>SearchFilters
+    <Flex bg="gray.100" p='4' justifyContent="center" flexWrap="wrap" >
         
-    </div>
+    </Flex>
   )
 }
 
