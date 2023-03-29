39438409da86a51e79de3983d53c9e89d877b7a4
kaiesmahmud <kaiesmahmud@gmail.com>
2023-03-29 18:06:21 +0600

    progress bar added

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,24 @@
 //Nextjs Element
 import Head from 'next/head'
+import { Router } from 'next/router'
 //components
 import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
 //chakra ui
 import { ChakraProvider } from '@chakra-ui/react'
+//nProgress
+import nProgress from 'nprogress'
 
 export default function App({ Component, pageProps }) {
+  nProgress.configure({ showSpinner : false });
+
+  Router.events.on( 'routeChangeStart' ,()=> {
+    nProgress.start();
+  })
+  Router.events.on( 'routeChangeComplete' ,()=> {
+    nProgress.done();
+  })
+
   return (
     <>
       <Head>
@@ -14,6 +26,8 @@ export default function App({ Component, pageProps }) {
         <meta name="description" content="RealState app is created by Nehal using nextjs(reactjs framework)" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        {/*--------- nprogress link ------------------ */}
+        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css' integrity='sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==' crossOrigin='anonymous' referrerPolicy='no-referrer' />
       </Head>
       <ChakraProvider>
         <header>
