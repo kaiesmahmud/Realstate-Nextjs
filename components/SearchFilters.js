3ed18ef43c504a5ddf42ee8e152e1aff7803d427
kaiesmahmud <kaiesmahmud@gmail.com>
2023-03-29 15:26:23 +0600

    search page create and data fetched successfully

---
+++ components/SearchFilters.js
@@ -0,0 +1,21 @@
+'use client'
+import React, { useEffect, useState } from 'react'
+//chakra ui
+import { Box, Flex, Text, Icon, Select, Spinner } from '@chakra-ui/react'
+//Next element
+import Image from 'next/image'
+import { useRouter } from 'next/router'
+//react-icons
+import {MdCancel} from 'react-icons/md'
+
+
+const SearchFilters = () => {
+    const rounter = useRouter();
+  return (
+    <div>SearchFilters
+        
+    </div>
+  )
+}
+
+export default SearchFilters
